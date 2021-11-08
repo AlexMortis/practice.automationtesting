44870265892c91bed273b4308f84748f6f60df67
Semencha Aleksandr Victorovich <semen4a@gmail.com>
2021-11-08 15:28:21 +0300

    file changes home and login_reg

--- login_regestration.py
+++ login_regestration.py
@@ -11,17 +11,17 @@
 # click_butn_register = driver.find_element_by_name("register").click()
 # driver.quit()
 
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium import webdriver
-# driver = webdriver.Chrome()
-# driver.maximize_window()
-# wait = WebDriverWait(driver, 5)
-# driver.get("http://practice.automationtesting.in/")
-# my_account = driver.find_element_by_link_text("My Account").click()
-# input_email = driver.find_element_by_id("username").send_keys("kaniviki@yandex.ru")
-# input_password = driver.find_element_by_id("password").send_keys("Twowrongsdon'tmakearight31")
-# click_butn_login = driver.find_element_by_name("login").click()
-# btn_logout_visible = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Logout")))
-# driver.quit()
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.maximize_window()
+wait = WebDriverWait(driver, 5)
+driver.get("http://practice.automationtesting.in/")
+my_account = driver.find_element_by_link_text("My Account").click()
+input_email = driver.find_element_by_id("username").send_keys("kaniviki@yandex.ru")
+input_password = driver.find_element_by_id("password").send_keys("Twowrongsdon'tmakearight31")
+click_butn_login = driver.find_element_by_name("login").click()
+btn_logout_visible = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Logout")))
+driver.quit()
